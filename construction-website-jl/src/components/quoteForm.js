8bff479283a504050bfe1d896db48fba205cdf7f
Jorge Alejandre <jorgealejandre.piki@gmail.com>
2025-01-27 22:58:18 -0800

    Created a new form input using formsubmit.co, simpler and uses captcha with no monthly limit on emails.

--- construction-website-jl/src/components/quoteForm.js
+++ construction-website-jl/src/components/quoteForm.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-import emailjs from 'emailjs-com';
 
 function QuoteForm() {
   const [formData, setFormData] = useState({
-    name: '',
-    email: '',
-    phone: '',
-    service: '',
-    message: '',
+    Name: '',
+    Email: '',
+    Phone: '',
+    Service: '',
+    Message: '',
     contactMethod: 'email',
   });
 
@@ -19,32 +18,25 @@ function QuoteForm() {
     });
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    emailjs.send('service_zxvnrvv', 'template_fzz3xra', formData, 'ohbO7yLv8xF9wsHH1')
-      .then((response) => {
-        console.log('SUCCESS', response.status, response.text);
-      }, (error) => {
-        console.log('FAILED.', error);
-      });
-  };
-
   return (
     <div className="max-w-lg mx-auto p-6 bg-white shadow-md rounded-lg">
-      <h1 className="text-center text-2xl" style={{ fontFamily: 'Montserrat, sans-serif' }}> Request a form</h1>
-      <form onSubmit={handleSubmit}>
+      <h1 className="text-center text-2xl" style={{ fontFamily: 'Montserrat, sans-serif' }}>Request a Quote</h1>
+
+      <form action="https://formsubmit.co/jorgealejandre.piki@gmail.com" method="POST">
+      <input type="hidden" name="_subject" value="New Quote Request!" />
+        <input type="hidden" name="_captcha" value="true" />
+        <input type="hidden" name="_template" value="table"/>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
             Name
           </label>
           <input
             id="name"
-            name="name"
+            name="Name"
             type="text"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Your name"
-            value={formData.name}
+            value={formData.Name}
             onChange={handleChange}
           />
         </div>
@@ -54,11 +46,11 @@ function QuoteForm() {
           </label>
           <input
             id="email"
-            name="email"
+            name="Email"
             type="email"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Your email"
-            value={formData.email}
+            value={formData.Email}
             onChange={handleChange}
           />
         </div>
@@ -68,11 +60,11 @@ function QuoteForm() {
           </label>
           <input
             id="phone"
-            name="phone"
+            name="Phone"
             type="tel"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Your phone number"
-            value={formData.phone}
+            value={formData.Phone}
             onChange={handleChange}
           />
         </div>
@@ -82,15 +74,15 @@ function QuoteForm() {
           </label>
           <select
             id="service"
-            name="service"
+            name="Service"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            value={formData.service}
+            value={formData.Service}
             onChange={handleChange}
           >
             <option value="">Select a service</option>
-            <option value="installation1">Installation Service 1</option>
-            <option value="installation2">Installation Service 2</option>
-            <option value="installation3">Installation Service 3</option>
+            <option value="Installation">Installation</option>
+            <option value="Maintenance">Maintenance</option>
+            <option value="Repair">Repair</option>
           </select>
         </div>
         <div className="mb-4">
@@ -99,26 +91,28 @@ function QuoteForm() {
           </label>
           <textarea
             id="message"
-            name="message"
+            name="Message"
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             placeholder="Your message"
-            value={formData.message}
+            value={formData.Message}
             onChange={handleChange}
           ></textarea>
         </div>
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2">Preferred Contact Method</label>
           <div className="flex items-center">
-            <input type="radio" id="contactEmail" name="contactMethod" value="email" className="mr-2" checked={formData.contactMethod === 'email'} onChange={handleChange} />
+            <input type="radio" id="contactEmail" name="Preferred Contact Method" value="Email" className="mr-2" checked={formData.contactMethod === 'email'} onChange={handleChange} />
             <label htmlFor="contactEmail" className="mr-4">Email</label>
-            <input type="radio" id="contactPhone" name="contactMethod" value="phone" className="mr-2" checked={formData.contactMethod === 'phone'} onChange={handleChange} />
+            <input type="radio" id="contactPhone" name="Preferred Contact Method" value="Phone" className="mr-2" checked={formData.contactMethod === 'phone'} onChange={handleChange} />
             <label htmlFor="contactPhone">Phone</label>
           </div>
         </div>
         <div className="flex items-center justify-between">
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" 
-          type="submit">
-          Submit
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          >
+            Submit
           </button>
         </div>
       </form>
