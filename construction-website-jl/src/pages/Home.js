46da7802e4f1b7c49da751b5a5fb02f681a9c621
Jorge Alejandre <jorgealejandre.piki@gmail.com>
2025-01-29 16:43:35 -0800

    Used library to add animation on scroll effect for touchup to homepage.

--- construction-website-jl/src/pages/Home.js
+++ construction-website-jl/src/pages/Home.js
@@ -5,6 +5,8 @@ import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPhone, faEnvelope, faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons';
 import QuoteForm from '../components/quoteForm';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 // Update the paths to the images
 const firstPic = '/photos/featuredFirst.jpg';
@@ -20,13 +22,17 @@ function Home() {
   }, []);
 
   useEffect(() => {
+    AOS.init({ duration: 1000 });
+  }, []);
+
+  useEffect(() => {
       window.scrollTo(0, 0);
     }, []);
     
   return (
     <div>
       {/* Backdrop with company information */}
-      <div className="relative bg-cover bg-center h-[600px]" style={{ backgroundImage: `url(${backgroundPhoto})` }}>
+      <div className="relative bg-cover bg-center h-[600px]" style={{ backgroundImage: `url(${backgroundPhoto})` }} data-aos="fade-up">
         <div className="absolute inset-0 bg-black opacity-5"></div>
         <div className="relative z-10 flex flex-col items-center justify-center h-full text-white bg-black bg-opacity-50 p-4 rounded-lg">
           <img src={logo} alt="Company logo" className="h-32 mb-4" />
@@ -35,17 +41,17 @@ function Home() {
         </div>
       </div>
 
-      <div className="mx-auto bg-gray-100">
+      <div className="mx-auto bg-gray-100" data-aos="fade-up">
         {/* Contact for quote form */}
         <QuoteForm />
       </div>
 
-      <div className="max-w-4xl mx-auto mt-8 ">
-        <h1 className="text-4xl font-bold mb-4 text-center" style={{ fontFamily: 'Montserrat, sans-serif' }} >FEATURED WORK</h1>
+      <div className="max-w-4xl mx-auto mt-8" data-aos="fade-up">
+        <h1 className="text-4xl font-bold mb-4 text-center" style={{ fontFamily: 'Montserrat, sans-serif' }}>FEATURED WORK</h1>
         {/* Carousel component from react-responsive-carousel meant to showcase some featured photos on the homepage */}
         <Carousel showThumbs={false} showArrows={false} showStatus={false} interval={4000} autoPlay infiniteLoop autoFocus={true} transitionTime={1000}
           className="rounded-lg overflow-hidden border-black border-2"
-          >
+        >
           <div>
             <img src={firstPic} alt="Cover with RV trailer" className="w-full h-92 object-cover" />
           </div>
@@ -61,7 +67,7 @@ function Home() {
         </Carousel>
 
         {/* Button to view more photos in the gallery */}
-        <div className="text-center mt-4">
+        <div className="text-center mt-4" data-aos="fade-up">
           <Link to="/gallery" className="no-underline">
             <button className='bg-white hover:text-white hover:bg-black text-black font-bold border-black border-2 py-2 px-4 rounded' style={{ fontFamily: 'Montserrat, sans-serif' }}>
               VIEW MORE
@@ -70,7 +76,7 @@ function Home() {
         </div>
 
         {/* Contact Information and Business Hours posted in separate divs */}
-        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="mt-8 grid grid-cols-1 md:grid-cols-2 gap-8" data-aos="fade-up">
           <div className="py-6 px-6">
             <h2 className="text-2xl font-bold mb-2">Contact Information</h2>
             <ul className="space-y-2 text-lg">
